83ed17b04d5985764ff90d46fca929b807fcb0a6
Adv1ce1 <135262373+Adv1ce1@users.noreply.github.com>
2023-08-09 12:52:11 +0300

    Update tables hw_1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,27 @@
--- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+  employee_id SERIAL PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  title VARCHAR(50),
+  birth_date DATE,
+  notes TEXT
+);
+
+CREATE TABLE customers
+(
+  customer_id VARCHAR(10) PRIMARY KEY,
+  company_name VARCHAR(40),
+  contact_name VARCHAR(40)
+);
+
+CREATE TABLE orders
+(
+  order_id SERIAL PRIMARY KEY,
+  customer_id VARCHAR(10),
+  employee_id INT,
+  order_date DATE,
+  ship_city VARCHAR(30),
+  FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+  FOREIGN KEY (employee_id) REFERENCES employees(employee_id)
+);
